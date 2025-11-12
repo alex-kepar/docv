55f40f1b6bf0b4ac1a7136b37ca61c7787687542
Oleksandr Kiporenko <alex.kepar@gmail.com>
2025-11-12 08:28:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
             targets: ["TruliooDocV", "TruliooCore", "TruliooSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.4.3"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.3"),
         .package(url: "https://github.com/connectrpc/connect-swift", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "TruliooDocV",
             dependencies: [
-                .product(name: "Lottie", package: "lottie-spm"),
+                .product(name: "Lottie", package: "lottie-ios"),
                 .product(name: "Connect", package: "connect-swift")
             ]
         ),
